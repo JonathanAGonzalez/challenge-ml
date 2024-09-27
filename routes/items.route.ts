e6da8a31950c35aa4fd9ad772e55fe05f5af3dfe
Jona <jonathang@hotmail.com.ar>
2024-09-27 15:49:04 -0300

    feat: add items route with getItems and getItem endpoints

---
+++ routes/items.route.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { getItems, getItem } from '../controllers/items.controller';
+const router = Router();
+
+router.get('/', getItems);
+router.get('/:id', getItem);
+
+export default router;
