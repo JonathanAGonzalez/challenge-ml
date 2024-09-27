2092debd8dc2699490042bc2cf838133a5b8699d
Jona <jonathang@hotmail.com.ar>
2024-09-27 15:46:27 -0300

    Initialize Express server with basic routing and health check endpoint

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+import itemsRouter from './routes/items.route';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/items', itemsRouter);
+
+app.get('/healt', (req: Request, res: Response) => {
+  res.json({ message: 'I am alive' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
